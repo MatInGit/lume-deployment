6ab400c04f05714b61fcd7a70355a8da4117db9a
MatInGit <mateo.leputa@googlemail.com>
2024-04-29 16:32:38 -0700

    added tests for p4p server and file interface

---
+++ tests/test_file_intefrace.py
@@ -0,0 +1,21 @@
+# from mm.interfaces.file_interface import h5dfInterface
+
+# def test_h5dfInterface_init():
+#     config = {
+#         "path": "tests/test_data/test.h5",
+#         "mode": "batch"
+#     }
+#     h5 = h5dfInterface(config)
+    
+# def test_h5dfInterface_load():
+#     config = {
+#         "path": "tests/test_data/test.h5",
+#         "mode": "batch"
+#     }
+#     h5 = h5dfInterface(config)
+#     data = h5.load()
+#     assert data is not None
+#     for key, value in data:
+#         assert key is not None
+#         assert value is not None
+    
---
+++ tests/test_p4p_server.py
@@ -0,0 +1,64 @@
+from mm.interfaces import SimlePVAInterfaceServer
+from mm.logging_utils.make_logger import get_logger, make_logger
+import numpy as np
+
+logger = make_logger("model_manager")
+
+
+
+def test_SimplePVAInterfaceServer_init():
+    config = {
+        "variables": {
+            "test": {
+                "name": "test",
+                "proto": "pva"
+            }
+        }
+    }
+    logger.info("Testing SimplePVAInterfaceServer init")
+    p4p = SimlePVAInterfaceServer(config)
+    p4p.close()
+    
+def test_SimplePVAInterfaceServer_put_and_get():
+    config = {
+        "variables": {
+            "test": {
+                "name": "test",
+                "proto": "pva"
+            }
+        }
+    }
+    logger.info("Testing SimplePVAInterfaceServer put")
+    p4p = SimlePVAInterfaceServer(config)
+    p4p.put("test", 1)
+    name, value_dict = p4p.get("test")
+    print(name, value_dict)
+    assert value_dict["value"] == 1
+    assert name == "test"
+    p4p.close()
+    
+def test_SimplePVAInterfaceServer_put_and_get_image():
+    config = {
+        "variables": {
+            "test": {
+                "name": "test",
+                "proto": "pva",
+                "type": "image",
+                "image_size": {"x": 10, "y": 10}
+            }
+        }
+    }
+    p4p = SimlePVAInterfaceServer(config)
+    
+    arry = np.ones((10, 10))
+    
+    p4p.put("test", arry)
+    name, value_dict = p4p.get("test")
+    print(name, value_dict)
+    print(value_dict["value"], type(value_dict["value"]))
+    assert value_dict["value"][0][0] == arry[0][0]
+    assert value_dict["value"].shape == arry.shape
+    assert name == "test"
+    p4p.close()
+    
+    
