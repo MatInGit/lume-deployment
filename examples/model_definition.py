040929772f35943719ad7faec80c66b6b31cfb03
MatInGit <mateo.leputa@googlemail.com>
2024-04-30 10:37:13 -0700

    changed example to do thresholding and added a line to install reqirements in docker

--- examples/model_definition.py
+++ examples/model_definition.py
@@ -18,5 +18,26 @@ class ExampleImageModel:
         output_dict["y_min"] = np.min(input_dict["image"])
         output_dict["y_mean"] = np.mean(input_dict["image"])
         output_dict["y_std"] = np.std(input_dict["image"])
-        output_dict["y_img"] = input_dict["image"] ** 2
+        output_dict["y_img"] = input_dict["image"]
+        
+        # square
+        output_dict["y_img"] = output_dict["y_img"] ** 2
+        
+        # threshhold 
+        max_val = np.max(output_dict["y_img"])
+        min_val = np.min(output_dict["y_img"])
+        threshhold = (max_val - min_val) * 0.5
+        # anythin below threshhold is set to min_val
+        output_dict["y_img"][output_dict["y_img"] < threshhold] = min_val
+        
+        # standardise between 0 and 1
+        # output_dict["y_img"] = (output_dict["y_img"] - output_dict["y_min"]) / (
+        #     output_dict["y_max"] - output_dict["y_min"]
+        # )
+        # # 255
+        # output_dict["y_img"] = output_dict["y_img"] * 255
+        
+        # ## square anything below 255*0.5
+        # output_dict["y_img"][output_dict["y_img"] < 255*0.5] = 255
+        
         return output_dict
