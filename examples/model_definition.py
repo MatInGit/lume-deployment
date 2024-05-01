6d4c877b25bd18dfba9117bac1aa8dcb4a2c772d
MatInGit <mateo.leputa@googlemail.com>
2024-05-01 12:29:11 -0700

    [FIX] fixed paths for example imports

---
+++ examples/model_definition.py
@@ -0,0 +1,10 @@
+from lume_model.base import LUMEBaseModel
+
+import numpy as np
+
+
+class ExampleModel(LUMEBaseModel):
+    def evaluate(self, input_dict):
+        output_dict = {}
+        output_dict["y"] = np.max([input_dict["x1"], input_dict["x2"]])
+        return output_dict
