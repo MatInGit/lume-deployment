ba80f423dc243074621d98d199081057a50bc083
MatInGit <mateo.leputa@googlemail.com>
2024-12-10 10:28:04 +0000

    tried adding profilinf to test but not very useful at this stage

--- tests/test_model_getter.py
+++ tests/test_model_getter.py
@@ -1,6 +1,6 @@
 from model_manager.src.model_utils.LocalModelGetter import LocalModelGetter
-
-
+import cProfile
+import os
 def test_LocalModelGetter():
     model_getter = LocalModelGetter({
         'model_path': 'tests/model/model_definition.py',
@@ -8,5 +8,14 @@ def test_LocalModelGetter():
     })
     model = model_getter.get_model()
     input_dict = {'x1': 1, 'x2': 2}
-    output_dict = model.evaluate(input_dict)
-    assert output_dict['y'] == 2
+    
+    for  i in range(100):
+        input_dict['x1'] = i
+        input_dict['x2'] = i*2
+        output_dict = model.evaluate(input_dict)
+        assert output_dict['y'] == max(input_dict['x1'], input_dict['x2'])
+# def test_profile():
+#     # Use runctx instead of run to provide the globals/locals context
+#     cProfile.runctx('test_LocalModelGetter()', globals(), locals(), 'tests/test_LocalModelGetter.prof')
+#     os.system('flameprof tests/test_LocalModelGetter.prof > tests/test_LocalModelGetter_flamegraph.html')
+# not particularly useful for this test case, but can be useful for more complex test cases
