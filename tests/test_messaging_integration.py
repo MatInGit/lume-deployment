f1d10bfa1832b047ac3c78c7606d92ebf51305e8
Leputa <CLRC\gbm96348@gaia0.isis.cclrc.ac.uk>
2025-02-18 15:04:40 +0000

    all test passing

--- tests/test_messaging_integration.py
+++ tests/test_messaging_integration.py
@@ -8,14 +8,19 @@ from model_manager.src.utils.messaging import (
 )
 from model_manager.src.transformers.BaseTransformers import SimpleTransformer
 from model_manager.src.interfaces import registered_interfaces
+import os
+import logging
 
 class MockModel:
     def __init__(self):
         self.predictions = []
 
     def predict(self, value):
-        # add x1 and x2
-        return {"pred": value['x1'] + value['x2']}
+        """our model expects a dictionary with {"name": {"value": value}, ...}"""
+        logging.info(f"Model received: {value}")
+        self.predictions.append(value)
+        # the next layer expects a dictionary with {"name": {"value": value}, ...} so we have to retrun it as follows   
+        return {"pred0": {"value": value['transformed']['value']['x1'] + value['transformed']['value']['x2']}}
 
 @pytest.fixture
 def message_broker():
@@ -46,6 +51,23 @@ def p4p_server():
     p4p = registered_interfaces['p4p_server'](config)
     yield p4p
     p4p.close()
+
+@pytest.fixture
+def p4p_server_out():
+    config = {
+        'variables': {
+            'out_scaled': {
+                'name': 'out_scaled',
+                'proto': 'pva',
+                'type': 'scalar',
+                'default': 0.0
+            }
+    }
+    }
+    
+    p4p = registered_interfaces['p4p_server'](config)
+    yield p4p
+    p4p.close()
     
     
 @pytest.fixture
@@ -57,7 +79,11 @@ def interface_observer(p4p_server):
     return InterfaceObserver(p4p_server, "in_interface")
 
 @pytest.fixture
-def transformer_observer():
+def interface_observer_out(p4p_server_out):
+    return InterfaceObserver(p4p_server_out, "out_interface")
+
+@pytest.fixture
+def transformer_observer_in():
     
     config1 = {
         'variables': {'x2': {'formula': 'A1 + B1'}, 'x1': {'formula': 'A1'}},
@@ -65,26 +91,51 @@ def transformer_observer():
     }
     return TransformerObserver(SimpleTransformer(config1), "in_transformer")
 
+
+@pytest.fixture
+def transformer_observer_out():
+    os.environ['PUBLISH']= 'True'
+    
+    config2 = {
+        'variables': {'out_scaled': {'formula': 'pred0 / 10.0'}},
+        'symbols': ['pred0'],
+    }
+    return TransformerObserver(SimpleTransformer(config2), "out_transformer",unpack_output=True)
+
 # we want to test if the data goes from interface -> transformer -> model
 
-def test_interface_observer_put(interface_observer, transformer_observer, model_observer, message_broker):
+def test_interface_observer_put(interface_observer, interface_observer_out, transformer_observer_in, transformer_observer_out, model_observer, message_broker,caplog):
+    caplog.set_level(logging.DEBUG)
     # Broker setup 
     message_broker.attach(interface_observer, "get_all")
-    message_broker.attach(transformer_observer, "in_interface")
+    message_broker.attach(transformer_observer_in, "in_interface")
     message_broker.attach(model_observer, "in_transformer")
+    message_broker.attach(transformer_observer_out, "model_out")
+    message_broker.attach(interface_observer_out, "out_transformer")
+    
 
     
     # get_all
-    message = Message(topic="get_all", source="get_all", key="key", value=None)
+    message = Message(topic="get_all", source="get_all", key="key", value={"dummy": {"value": 1}})
     
     # this should trigger the interface to get all variables
     message_broker.notify(message)
     assert len(message_broker.queue) == 2
+    logging.info(message_broker.queue)
     for message in message_broker.queue:
         assert message.topic == "in_interface"
     message_broker.parese_queue()
     assert len(message_broker.queue) == 1
+    logging.info(message_broker.queue)
     assert message_broker.queue[0].topic == "in_transformer"
     message_broker.parese_queue()
     assert len(message_broker.queue) == 1
+    logging.info(message_broker.queue)
     assert message_broker.queue[0].topic == "model_out"
+    message_broker.parese_queue()
+    assert len(message_broker.queue) == 1
+    logging.info(message_broker.queue)
+    assert message_broker.queue[0].topic == "out_transformer"
+    assert message_broker.queue[0].value['out_scaled']['value'] == 0.5 # if you are looking at this and wondering why it returns a different shape looks for the unpack_output=True in the transformer_observer_out fixture
+    message_broker.parese_queue()
+    assert len(message_broker.queue) == 0 # no messahes left in the queue
