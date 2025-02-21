74f355a78869bd5562455501fb7e80e254d8fcd0
MatInGit <mateo.leputa@googlemail.com>
2025-02-21 14:28:49 +0000

    Refactor config parser and update test cases with new YAML schema

--- model_manager/src/config/config_object.py
+++ model_manager/src/config/config_object.py
@@ -7,142 +7,182 @@ from enum import Enum
 from matplotlib import pyplot as plt
 from uuid import uuid4
 import os
-
+import logging
 allowed_transformers = list(registered_transformers.keys())
 
 
-# all of the below objects need more work
+# # all of the below objects need more work
+
+
+
+# class InputDataConfig(pydantic.BaseModel):
+#     get_method: str
+#     config: Any
+
+
+# class InputDataToModelConfig(pydantic.BaseModel):
+#     type: str
+#     config: Any
+
+#     @pydantic.field_validator('type')
+#     def check_type(cls, v):
+#         if v not in allowed_transformers:
+#             raise ValueError(
+#                 f'Invalid transformer type: {v}, choose from {allowed_transformers}'
+#             )
+#         return v
+
+
+# class ModelToOutputDataConfig(pydantic.BaseModel):
+#     type: str
+#     config: Any
+
+#     @pydantic.field_validator('type')
+#     def check_type(cls, v):
+#         if v not in allowed_transformers:
+#             raise ValueError(
+#                 f'Invalid transformer type: {v}, choose from {allowed_transformers}'
+#             )
+#         return v
+
+
+# class OutputDataToConfig(pydantic.BaseModel):
+#     put_method: str
+#     config: Any
+
+
+# class OutputModelConfig(pydantic.BaseModel):
+#     config: Any
+    
+    
+# class allowedRoutingTypes(Enum):
+#     in_interface: str = "in_interface"
+#     in_transformer: str = "in_transformer"
+#     model: str = "model"
+#     out_transformer: str = "out_transformer"
+#     out_interface: str = "out_interface"
+#     model_evaluator: str = "model_evaluator"
+#     misc: str = "misc"
+    
+# class RoutingObject(pydantic.BaseModel):
+#     type: allowedRoutingTypes
+#     pub: Optional[str] = None
+#     sub: Optional[str] = None
+#     args: Optional[dict[str, Union[str, dict, bool]]] | str = None
+
+
+# class RoutingConfig(pydantic.BaseModel):
+#     config: dict[str, RoutingObject]
+    
+#     class Config:
+#         arbitrary_types_allowed = True # to allow nx.DiGraph
+    
+#     @computed_field(return_type=nx.DiGraph)
+#     @property
+#     def graph(self):
+#         G = nx.DiGraph()
+#         # pubs are nodes and subs are edges
+#         nodes = []
+#         edges = []
+#         for key, value in self.config.items():
+#             nodes.append(value.pub)
+#             nodes.append(value.sub)
+#             edges.append((value.sub, value.pub))
+
+#         # keep only unique nodes
+#         nodes = list(set(nodes))
+#         # remove none values
+#         nodes = [x for x in nodes if x is not None]
+#         # remove none values
+#         edges = [x for x in edges if x[0] is not None and x[1] is not None]
+#         G.add_nodes_from(nodes)
+#         G.add_edges_from(edges)
+#         return G
+        
+        
+#     @pydantic.field_validator('graph')
+#     def check_routing(cls, v):
+#         isolated_nodes = list(nx.isolates(v))
+#         if isolated_nodes:
+#             raise ValueError(
+#                 f'Isolated nodes found in routing graph: {isolated_nodes}'
+#             )
+#         return v
+
+# above is legacy code, below is the new simplified version
+
+class ModuleConfig(pydantic.BaseModel):
+    name: str
+    pub: Optional[str] = None
+    sub: Optional[Union[str, list]] = None
+    module_args: Optional[Union[dict[str, Union[str, dict, bool]], str]] = None
+    config: Any = None # kind of a free for all for now but we can narrow down the specifics later
+
+    @pydantic.field_validator('module_args', mode = "before")
+    def validate_module_args(cls, v):
+        if isinstance(v, str):
+            return {}
+        return v
+
+    
 class DeploymentConfig(pydantic.BaseModel):
     type: str
 
 
-class InputDataConfig(pydantic.BaseModel):
-    get_method: str
-    config: Any
-
-
-class InputDataToModelConfig(pydantic.BaseModel):
-    type: str
-    config: Any
-
-    @pydantic.field_validator('type')
-    def check_type(cls, v):
-        if v not in allowed_transformers:
-            raise ValueError(
-                f'Invalid transformer type: {v}, choose from {allowed_transformers}'
-            )
-        return v
-
-
-class ModelToOutputDataConfig(pydantic.BaseModel):
-    type: str
-    config: Any
-
-    @pydantic.field_validator('type')
-    def check_type(cls, v):
-        if v not in allowed_transformers:
-            raise ValueError(
-                f'Invalid transformer type: {v}, choose from {allowed_transformers}'
-            )
-        return v
-
-
-class OutputDataToConfig(pydantic.BaseModel):
-    put_method: str
-    config: Any
-
-
-class OutputModelConfig(pydantic.BaseModel):
-    config: Any
-    
-    
-class allowedRoutingTypes(Enum):
-    in_interface: str = "in_interface"
-    in_transformer: str = "in_transformer"
-    model: str = "model"
-    out_transformer: str = "out_transformer"
-    out_interface: str = "out_interface"
-    model_evaluator: str = "model_evaluator"
-    misc: str = "misc"
-    
-class RoutingObject(pydantic.BaseModel):
-    type: allowedRoutingTypes
-    pub: Optional[str] = None
-    sub: Optional[str] = None
-    args: Optional[dict[str, Union[str, dict, bool]]] | str = None
-
-
-class RoutingConfig(pydantic.BaseModel):
-    config: dict[str, RoutingObject]
-    
+class ConfigObject(pydantic.BaseModel):
+    deployment: DeploymentConfig
+    modules: dict[str, ModuleConfig]
     class Config:
         arbitrary_types_allowed = True # to allow nx.DiGraph
-    
+        
+
     @computed_field(return_type=nx.DiGraph)
     @property
     def graph(self):
         G = nx.DiGraph()
-        # pubs are nodes and subs are edges
         nodes = []
         edges = []
-        for key, value in self.config.items():
-            nodes.append(value.pub)
-            nodes.append(value.sub)
-            edges.append((value.sub, value.pub))
-
-        # keep only unique nodes
+        # to collect edges we need to go through each item , look at what its publishing and find matching subsribers, this will form an edge
+        for key, value in self.modules.items():
+            if isinstance(value.pub, str):
+                    value.pub = [value.pub]
+            if value.pub is not None and value.pub != []:
+                for key2, value2 in self.modules.items():
+                    if value2.sub is not None:
+                        # normalise them all to lists
+                        if isinstance(value2.sub, str):
+                            value2.sub = [value2.sub]
+                            
+                        for pub in value.pub:
+                            for sub in value2.sub:
+                                if pub == sub and value.name != value2.name:
+                                    edges.append((
+                                        value.name,
+                                        value2.name
+                                    ))
+                                
+            nodes.append(value.name)
+            
+        # logging.debug(f"Nodes: {nodes}")
+        # logging.debug(f"Edges: {edges}")
         nodes = list(set(nodes))
-        # remove none values
         nodes = [x for x in nodes if x is not None]
-        # remove none values
-        edges = [x for x in edges if x[0] is not None and x[1] is not None]
+        edges = [tuple(x) for x in edges]
+        
         G.add_nodes_from(nodes)
         G.add_edges_from(edges)
-        return G
-        
-        
-    @pydantic.field_validator('graph')
-    def check_routing(cls, v):
-        isolated_nodes = list(nx.isolates(v))
+        isolated_nodes = list(nx.isolates(G))
+        logging.debug(f"Isolated nodes: {isolated_nodes}")
         if isolated_nodes:
             raise ValueError(
-                f'Isolated nodes found in routing graph: {isolated_nodes}'
+                f'Isolated nodes found in routing graph: {isolated_nodes}, this means that a modules is niether a publisher or subscriber to any other module'
             )
-        return v
-
-class ValidModuleConfig(Enum):
-    input_data: InputDataConfig = InputDataConfig
-    input_data_to_model: InputDataToModelConfig = InputDataToModelConfig
-    output_model_to_data: ModelToOutputDataConfig = ModelToOutputDataConfig
-    output_data_to: OutputDataToConfig = OutputDataToConfig
-    outputs_model: OutputModelConfig = OutputModelConfig
-            
-
-class ConfigObject(pydantic.BaseModel):
-    routing: Optional[RoutingConfig] = None
-    deployment: DeploymentConfig
-    # old 
-    # input_data: Optional[dict[str, InputDataConfig]] = None
-    # input_data_to_model: Optional[dict[str, InputDataToModelConfig]] = None
-    # output_model_to_data: Optional[dict[str, ModelToOutputDataConfig]] = None
-    # output_data_to: Optional[dict[str, OutputDataToConfig]] = None
-    # outputs_model: Optional[dict[str, OutputModelConfig]] = None
-    
-    # new all of these ara a module config
-    modules: dict[str, ValidModuleConfig]
-
-    
+        return G
     
 
-    @model_validator(mode='before')
-    @classmethod
-    def set_default_routing(cls, values):
-        if 'routing' not in values:
-            values['routing'] = makeDefaultRoutingConfig()
-        return values
     
     def draw_routing_graph(self):
-        G = self.routing.graph
+        G = self.graph
         plt.figure(figsize=(10, 10))
         nx.draw(G, with_labels=True)
         if not os.path.exists("./graphs"):
