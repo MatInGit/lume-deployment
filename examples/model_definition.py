90337839fe9e5141cd786e80e08519304721ddf1
MatInGit <CLRC\gbm96348@gaia0.isis.cclrc.ac.uk>
2025-05-15 15:30:10 +0100

    moved things around to clean up the repo, emptied out the examples dir

--- examples/model_definition.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from lume_model.base import LUMEBaseModel
-
-
-class ExampleModel(LUMEBaseModel):
-    def evaluate(self, input_dict):
-        output_dict = {}
-        output_dict['y'] = np.max([input_dict['x1'], input_dict['x2']])
-        return output_dict
