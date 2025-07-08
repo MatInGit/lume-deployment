ba2d3e3b36c82f3edf5fd0138097f5eaf181bed5
MatInGit <CLRC\gbm96348@gaia0.isis.cclrc.ac.uk>
2025-07-08 14:12:23 +0100

    added intial docs

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Poly-Lithic'
+copyright = '2025, Matuesz Leputa'
+author = 'Matuesz Leputa'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google/NumPy docstrings
+    'sphinx_autodoc_typehints',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# Optional theme
+html_theme = 'furo'  # or 'alabaster', 'sphinx_rtd_theme', etc.
+html_static_path = ['_static']
