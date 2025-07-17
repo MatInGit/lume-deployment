a617b859c62757246599cfe59d31b0d8c6ba8031
MatInGit <mateo.leputa@googlemail.com>
2025-07-17 16:41:50 +0100

    added typing to abstract class

--- model_manager/src/transformers/BaseTransformer.py
+++ model_manager/src/transformers/BaseTransformer.py
@@ -2,21 +2,21 @@ from abc import ABC, abstractmethod
 
 class BaseTransformer:
     @abstractmethod
-    def __init__(self, config):
+    def __init__(self, config: dict):
         """
-        config: dict passed from the pv_mappings.yaml files
+        config: dict passed from the pv_mappings.yaml files.
         """
         pass
     
     @abstractmethod
     def transform(self):
         """
-        Initiate transform function to transform the input data
+        Call transform function to transform the input data, see SimpleTransformer in model_manager/src/transformers/BaseTransformers.py for an example.
         """
         pass
     
     @abstractmethod
-    def handler(self, pv_name, value):
+    def handler(self, pv_name: str, value: dict | float | int):
         """
         Handler function to handle the input data, in most cases it initiates the transform function when all the input data is available. 
         Handler is the only function exposed to the main loop of the program aside from initial configuration.
