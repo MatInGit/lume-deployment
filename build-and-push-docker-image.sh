29fd2baac6835b014b38c599bb58773b0727bccf
MatInGit <mateo.leputa@googlemail.com>
2025-07-17 16:41:46 +0100

    dockerfile updates

--- build-and-push-docker-image.sh
+++ build-and-push-docker-image.sh
@@ -1,22 +1,22 @@
 # docker login harbor.stfc.ac.uk
 # set build time variables
-export BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-export VCS_REF=$(git rev-parse --short HEAD)
+# export BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+# export VCS_REF=$(git rev-parse --short HEAD)
 
-# if build-info file exists, remove it
-if [ -f build-info ]; then
-    rm build-info.json
-fi
+# # if build-info file exists, remove it
+# if [ -f build-info ]; then
+#     rm build-info.json
+# fi
 
-# create build-info file
-touch build-info.json
-echo "{" >> build-info.json
-echo "  \"build-date\": \"$BUILD_DATE\"," >> build-info.json
-echo "  \"vcs-ref\": \"$VCS_REF\"" >> build-info.json
-echo "}" >> build-info.json
+# # create build-info file
+# touch build-info.json
+# echo "{" >> build-info.json
+# echo "  \"build-date\": \"$BUILD_DATE\"," >> build-info.json
+# echo "  \"vcs-ref\": \"$VCS_REF\"" >> build-info.json
+# echo "}" >> build-info.json
 
-docker build --pull --rm -f "Dockerfile" -t harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:dev "." &&
-docker push harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:dev
+docker build --pull --rm -f "Dockerfile" -t harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:dev10 "." &&
+docker push harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:dev10
 
-docker build --pull --rm -f "Dockerfile.interactive" -t harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:interactive "." &&
-docker push harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:interactive
+docker build --pull --rm -f "Dockerfile.interactive" -t harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:interactive10 "." &&
+docker push harbor.stfc.ac.uk/isis-accelerator-controls/lume-deploy:interactive10
